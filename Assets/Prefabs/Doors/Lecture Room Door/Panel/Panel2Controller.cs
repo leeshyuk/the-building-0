b7ab3d6d845509e0b16f8820759a725a972fba94
leeshyuk <leesh0313@hufs.ac.kr>
2024-05-10 12:14:14 +0900

    Panel Controller 스크립트들 수정

--- Assets/Prefabs/Doors/Lecture Room Door/Panel/Panel2Controller.cs
+++ Assets/Prefabs/Doors/Lecture Room Door/Panel/Panel2Controller.cs
@@ -12,6 +12,8 @@
  *      1.  원하는 각도로 정확히 움직이는 것은 불가능 (유니티의 모든 움직임이 이러함)
  *          연속된 값의 변화(움직임)이라도 결국 디지털값이라서 간격이 존재함
  *          속도가 빠르면 간격이 커지기 때문에 빠른 속도에서는 이상하게 보일 수 있음
+ *          
+ *      2.  상위 프리팹 Lecture Room Door의 회전과 동기화되지 않음
  *
  */
 using UnityEngine;
@@ -27,7 +29,6 @@ public class PanelController : MonoBehaviour
     {
         // transform.eulerAngles[1]는 Transform 컴포넌트의 Rotation의 현재 Y값을 의미
         initialAngle = transform.eulerAngles[1]; // 문이 닫혀있을 때의 각도
-        print(initialAngle);
     }
 
     private void Update()
@@ -37,17 +38,16 @@ public class PanelController : MonoBehaviour
         if (angle > 90f) angle = 90f;
 
         // 현재 각도
-        // 문이 열리는 것이 각도가 작아지는 것이라서 360에서 빼야함
-        float currentAngle = 360 - transform.eulerAngles[1];
-        if (currentAngle == 360) currentAngle = 0f;
+        float currentAngle = initialAngle - transform.eulerAngles[1];
+        if (currentAngle < 0f) currentAngle += 360;
 
         // 현재 각도가 목표하는 각도보다 작을 때 (- 1은 허용하는 오차 범위)
-        if (currentAngle < angle + initialAngle - 1) transform.Rotate(Vector3.down, speed * Time.deltaTime); // 열기
+        if (currentAngle < angle - 1) transform.Rotate(Vector3.down, speed * Time.deltaTime); // 열기
 
         // 현재 각도가 목표하는 각도보다 클 때 (+ 1은 허용하는 오차 범위)
-        else if (currentAngle > angle + initialAngle + 1) transform.Rotate(Vector3.up, speed * Time.deltaTime); // 닫기
+        else if (currentAngle > angle + 1) transform.Rotate(Vector3.up, speed * Time.deltaTime); // 닫기
 
         // 각도가 범위를 벗어날 때, 오차로 인해 발생
-        if (currentAngle > 90f + initialAngle || currentAngle < initialAngle) transform.rotation = Quaternion.Euler(0f, angle + initialAngle, 0f); // 목표 각도로 바로 지정
+        if (currentAngle < 0f || 90f < currentAngle) transform.rotation = Quaternion.Euler(0f, - angle + initialAngle, 0f); // 목표 각도로 바로 지정
     }
 }
